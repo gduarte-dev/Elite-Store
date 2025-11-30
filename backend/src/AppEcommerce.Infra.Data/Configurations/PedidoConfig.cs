d769993306d45e01a052e1905f036de10bb0a509
gduarte-dev <Gabriel.Duarte.dev.rj@gmail.com>
2025-11-30 15:08:05 -0300

    Add PedidoConfig for PedidoEntity configuration

---
+++ backend/src/AppEcommerce.Infra.Data/Configurations/PedidoConfig.cs
@@ -0,0 +1,33 @@
+using AppEcommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AppEcommerce.Infra.Data.Configurations
+{
+    public class PedidoConfig : IEntityTypeConfiguration<PedidoEntity>
+    {
+        public void Configure(EntityTypeBuilder<PedidoEntity> builder)
+        {
+            builder.ToTable("Pedidos");
+
+            builder.HasKey(p => p.IdPedido);
+
+            builder.Property(p => p.IdPedido)
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(p => p.IdCliente)
+                   .IsRequired();
+
+            builder.Property(p => p.DataPedido)
+                   .IsRequired();
+
+            builder.Property(p => p.ValorTotal)
+                   .HasColumnType("decimal(18,2)");
+
+            builder.HasMany(p => p.Itens)
+                   .WithOne(i => i.Pedido)
+                   .HasForeignKey(i => i.IdPedido)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
